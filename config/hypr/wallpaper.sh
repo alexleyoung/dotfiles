4d4398dbc2051d3b8adbef15b6e2f888f8414644
Alex Young <>
2025-05-20 00:11:47 -0500

    add wallpaper fix in hypr, kind of hacky sorry

---
+++ config/hypr/wallpaper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Wait until all monitors are registered (adjust timeout as needed)
+timeout=5
+count=0
+
+# Wait until we have more than 1 output (or however many you expect)
+while [ $(hyprctl monitors | grep 'Monitor' | wc -l) -lt 2 ] && [ $count -lt $timeout ]; do
+    sleep 0.5
+    count=$((count + 1))
+done
+
+# Set wallpaper for each monitor
+swww img /home/alexy/Documents/wallpapers/singapore-wp.png --transition-type any
+
